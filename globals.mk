9ba544ce803a66fde9e9ac75c5914176637bdc66
Matthew Naylor <mattfn@gmail.com>
2016-12-22 18:47:16 +0000

    Make files

---
+++ globals.mk
@@ -0,0 +1,17 @@
+# Common directories
+INC = $(realpath $(TINSEL_ROOT)/include)
+QP  = $(realpath $(TINSEL_ROOT)/de5)
+RTL = $(realpath $(TINSEL_ROOT)/rtl)
+BIN = $(realpath $(TINSEL_ROOT)/bin)
+
+# RISC-V tools
+RV_ARCH    = RV32I
+RV_CC      = riscv64-unknown-elf-gcc
+RV_LD      = riscv64-unknown-elf-ld
+RV_OBJCOPY = riscv64-unknown-elf-objcopy
+
+# Extend PATH
+export PATH := $(PATH):$(realpath $(BIN))
+
+# Set path to config script
+export CONFIG := $(realpath $(TINSEL_ROOT)/config.py)
--- include/make.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo -e '#ifndef _CONFIG_H_' > config.h
-echo -e '#define _CONFIG_H_\n' >> config.h
-python ../config.py cpp >> config.h
-echo -e '\n#endif' >> config.h
