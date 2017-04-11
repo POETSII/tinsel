c213844d6856943c30966136907af83cd5cd2282
Matthew Naylor <mattfn@gmail.com>
2017-04-11 20:06:29 +0100

    Update to heat app

--- apps/heat/heat-v1.c
+++ apps/heat/heat-v1.c
@@ -1,7 +1,14 @@
+// Simulate heat diffusion on a LENxLEN grid of threads.
+// Each thread handles an 8x8 subgrid of cells.
+#define LEN 32
+
 #include <tinsel.h>
 
-// Simulate Newton's law of cooling on a 2D square grid of threads.
-// Each thread simulates a subgrid of LxL cells.
+// 32-bit fixed-point number in 16.16 format
+#define FixedPoint(x, y) (((x) << 16) | (y))
+
+// Poles
+// -----
 
 // Direction: north, south, east, and west
 typedef enum {N=0, S=1, E=2, W=3} Dir;
@@ -15,36 +22,105 @@ inline Dir opposite(Dir d)
   return E;
 }
 
-// 32-bit fixed-point number in 16.16 format
-#define FixedPoint(x, y) (((x) << 16) | (y))
+// Mapping subgrids to threads and back
+// ------------------------------------
+
+// Thread with id T gets mapped to subgrid with coords X and Y where X
+// is the even bits of T and Y is the odd bits of T.  This keeps
+// neighbouring subgrids near to each other in the hardware.
+
+// Return bits at even-numbered indexes
+uint32_t evens(uint32_t in)
+{
+  uint32_t out = 0;
+  for (int i = 0; i < 16; i++) {
+    out = out | ((in&1) << i);
+    in >>= 2;
+  }
+  return out;
+}
+
+// Return bits at odd-numbered indexes
+uint32_t odds(uint32_t in)
+{
+  return evens(in >> 1);
+}
+
+// Determine X and Y coords of thread's subgrid
+void myXY(int* x, int* y)
+{
+  int me = tinselId();
+  *x = evens(me);
+  *y = odds(me);
+}
+
+// Partial inverse of evens
+uint32_t unevens(uint32_t in)
+{
+  uint32_t out = 0;
+  for (int i = 0; i < 16; i++) {
+    out = out | ((in&1) << (2*i));
+    in >>= 1;
+  }
+  return out;
+}
+
+// Partial inverse of odds
+uint32_t unodds(uint32_t in)
+{
+  return unevens(in) << 1;
+}
+
+// Determine thread id from X and Y coords of subgrid
+int fromXY(int x, int y)
+{
+  return unevens(x) | unodds(y);
+}
+
+// Message format
+// --------------
+
+typedef struct {
+  // The time step that the sender is on
+  int time;
+  // Which direction did the message come from?
+  Dir from;
+  // Temperatures from the sender
+  int temp[8];
+} Msg;
+
+// Top-level
+// =========
 
 int main()
 {
-  // Id for this thread
-  const uint32_t me = tinselId();
+  // Subgrid memory
+  // --------------
+
+  // Space for two subgrids (the current state and the next state)
+  int subgridSpace[8][8];
+  int newSubgridSpace[8][8];
+
+  // Pointers to subgrids, used in a double-buffer fashion
+  int (*subgrid)[8] = subgridSpace;
+  int (*newSubgrid)[8] = newSubgridSpace;
+  int (*subgridPtr)[8];
+
+  // Subgrid location
+  // ----------------
+
+  int xPos, yPos;
+  myXY(&xPos, &yPos);
 
   // Neighbours
   // ----------
-  //
-  // Logically, there is a square grid of threads.  (We assume
-  // TinselLogThreadsPerBoard is even, otherwise grid may not
-  // be square.)
-
-  // Square length
-  const uint32_t logLen = TinselLogThreadsPerBoard >> 1;
-  const uint32_t len    = 1 << logLen;
-
-  // X and Y position of thread in grid
-  const uint32_t xPos = me & (len-1);
-  const uint32_t yPos = me >> logLen;
 
   // The thread to the north, south, east, and west
-  // (-1 denotes no such thread, i.e. at the edges of the grid)
   int neighbour[4];
-  neighbour[N] = yPos == 0     ? -1 : (((yPos-1) << logLen) | xPos);
-  neighbour[S] = yPos == len-1 ? -1 : (((yPos+1) << logLen) | xPos);
-  neighbour[E] = xPos == len-1 ? -1 : ((yPos << logLen) | (xPos+1));
-  neighbour[W] = xPos == 0     ? -1 : ((yPos << logLen) | (xPos-1));
+  neighbour[N] = yPos == 0     ? -1 : fromXY(xPos, yPos-1);
+  neighbour[S] = yPos == LEN-1 ? -1 : fromXY(xPos, yPos+1);
+  neighbour[E] = xPos == LEN-1 ? -1 : fromXY(xPos+1, yPos);
+  neighbour[W] = xPos == 0     ? -1 : fromXY(xPos-1, yPos);
 
   // List containing each neighbour
   // (Only the first numNeighbours elements are valid)
@@ -53,181 +129,152 @@ int main()
   for (int i = 0; i < 4; i++)
     if (neighbour[i] >= 0) neighbourList[numNeighbours++] = i;
 
-  // Subgrids
-  // --------
-  //
-  // Each thread simulates a square subgrid of LxL cells.
-
-  // We pick a subgrid length small enough for a whole subgrid edge to
-  // be sent in a single message, with 1 word leftover to specify the
-  // direction from which the message came.
-  const uint32_t L = (1 << TinselLogWordsPerMsg) - 1;
-
-  // We allocate space for two subgrids (the current state and the
-  // next state) on the stack.
-  int subgridSpace[L][L];
-  int newSubgridSpace[L][L];
-
-  // Pointers to subgrids, used in a double-buffer fashion
-  int (*subgrid)[L] = subgridSpace;
-  int (*newSubgrid)[L] = newSubgridSpace;
-  int (*gridPtr)[L];
-
   // Initial state
   // -------------
 
-  // Edge temperatures to be sent to neighbours
-  volatile int* edgeOut[4];
-  for (int i = 0; i < 4; i++) edgeOut[i] = tinselSlot(i);
+  // Messages to be sent to neighbours
+  volatile Msg* msgOut[4];
+  for (int i = 0; i < 4; i++) msgOut[i] = tinselSlot(i);
 
-  // Edge temperatures received from neighbours
-  volatile int* edgeIn[4];
-  for (int i = 4; i < 8; i++) edgeIn[i-4] = tinselSlot(i);
+  // Messages received from neighbours
+  volatile Msg* msgIn[4];
+  for (int i = 0; i < 4; i++) {
+    msgIn[i] = tinselSlot(i+4);
+    if (neighbour[i] >= 0) tinselAlloc(msgIn[i]);
+  }
 
-  // Buffer for edge temperatures received from neighbours
+  // Buffer for messages received from neighbours
   // (At most two edges from the same neighbour can await processing
   // at any time, hence the need for this buffer)
-  volatile int* edgeInBuffer[4];
+  volatile Msg* msgInBuffer[4];
   for (int i = 0; i < 4; i++) {
-    edgeInBuffer[i] = 0;
+    msgInBuffer[i] = 0;
     tinselAlloc(tinselSlot(i+8));
   }
 
   // Zero initial subgrid
-  for (int i = 0; i < L; i++)
-    for (int j = 0; j < L; j++)
+  for (int i = 0; i < 8; i++)
+    for (int j = 0; j < 8; j++)
       subgrid[i][j] = 0;
 
-  // Zero initial edges
-  for (int i = 0; i < 4; i++)
-    for (int j = 1; j <= L; j++)
-      edgeIn[i][j] = 0;
+  // Zero output messages
+  for (int d = 0; d < 4; d++)
+    for (int i = 0; i < 8; i++)
+      msgOut[d]->temp[i] = 0;
 
   // Apply heat at north edge
-  if (yPos == 0)
-    for (int i = 1; i <= L; i++)
-      edgeIn[N][i] = FixedPoint(255, 0);
+  if (neighbour[N] < 0)
+    for (int i = 0; i < 8; i++)
+      msgIn[N]->temp[i] = FixedPoint(255, 0);
 
   // Apply heat at west edge
-  if (xPos == 0)
-    for (int i = 1; i <= L; i++)
-      edgeIn[W][i] = FixedPoint(255, 0);
+  if (neighbour[W] < 0)
+    for (int i = 0; i < 8; i++)
+      msgIn[W]->temp[i] = FixedPoint(255, 0);
 
   // Apply cool at south edge
-  if (yPos == (len-1))
-    for (int i = 1; i <= L; i++)
-      edgeIn[S][i] = FixedPoint(40, 0);
+  if (neighbour[S] < 0)
+    for (int i = 0; i < 8; i++)
+      msgIn[S]->temp[i] = FixedPoint(40, 0);
 
   // Apply cool at east edge
-  if (xPos == (len-1))
-    for (int i = 1; i <= L; i++)
-      edgeIn[E][i] = FixedPoint(40, 0);
+  if (neighbour[E] < 0)
+    for (int i = 0; i < 8; i++)
+      msgIn[E]->temp[i] = FixedPoint(40, 0);
 
-  // Messages will be comprised for 4 flits
-  tinselSetLen(3);
+  // Messages will be comprised of 3 flits
+  tinselSetLen(2);
 
   // Simulation
   // ----------
 
+  // Track number of messages received and sent for current time step
+  int received = 0;
+  int sent = 0;
+
   // Number of time steps to simulate
-  const int nsteps = 1000;
+  const int nsteps = 100000;
 
   // Simulation loop
   for (int t = 0; t < nsteps; t++) {
+   // Send/receive loop
+    while (received < numNeighbours || sent < numNeighbours) {
+      // Compute wait condition
+      TinselWakeupCond cond =
+         TINSEL_CAN_RECV | (sent < numNeighbours ? TINSEL_CAN_SEND : 0);
 
-    // Ensure no incomplete sends before continuing
-    // (Don't want to modify edgeOut until all edges have been sent)
-    tinselWaitUntil(TINSEL_CAN_SEND);
+      // Wait to send or receive message
+      tinselWaitUntil(cond);
 
-    // Update state
-    for (int y = 0; y < L; y++) {
-      for (int x = 0; x < L; x++) {
-        // Values of left, right, above, and below cells
-        int l = x == 0     ? edgeIn[W][y+1] : subgrid[y][x-1];
-        int r = x == (L-1) ? edgeIn[E][y+1] : subgrid[y][x+1];
-        int a = y == 0     ? edgeIn[N][x+1] : subgrid[y-1][x];
-        int b = y == (L-1) ? edgeIn[S][x+1] : subgrid[y+1][x];
-        // Average of surrounding temperatures
-        int surroundings = (a + b + l + r) >> 2;
-        // Compute new temperature for this cell
-        // (Assuming dissapation constant of 0.25)
-        int newTemp =
-          subgrid[y][x] - (subgrid[y][x] - surroundings);
-        // Update subgrid
-        newSubgrid[y][x] = newTemp;
-        // Update edges
-        if (y == 0)   edgeOut[N][x+1] = newTemp;
-        if (y == L-1) edgeOut[S][x+1] = newTemp;
-        if (x == 0)   edgeOut[W][y+1] = newTemp;
-        if (x == L-1) edgeOut[E][y+1] = newTemp;
-      }
-    }
-
-    // Allocate space to receive edges
-    for (int i = 0; i < numNeighbours; i++) {
-      Dir d = neighbourList[i];
-      tinselAlloc(edgeIn[d]);
-    }
-
-    // Counts of edges received and sent
-    int edgesSent = 0;
-    int edgesReceived = 0;
-
-    // Recognise any buffered edges
-    for (int i = 0; i < 4; i++)
-      if (edgeInBuffer[i] != 0) {
-        edgeIn[i] = edgeInBuffer[i];
-        edgeInBuffer[i] = 0;
-        edgesReceived++;
+      // Send edge temperatures
+      if (sent < numNeighbours && tinselCanSend()) {
+        int dir = neighbourList[sent];
+        // Send edge to neighbour
+        msgOut[dir]->time = t;
+        msgOut[dir]->from = opposite(dir);
+        tinselSend(neighbour[dir], msgOut[dir]);
+        // Move on to next neighbour
+        sent++;
       }
 
-    // Send & receive new edges
-    for (;;) {
-      uint32_t needToSend = edgesSent < numNeighbours;
-      uint32_t needToRecv = edgesReceived < numNeighbours;
-      if (!needToSend && !needToRecv) break;
-      uint32_t waitCond = (needToSend ? TINSEL_CAN_SEND : 0)
-                        | (needToRecv ? TINSEL_CAN_RECV : 0);
-
-      // Suspend thread
-      tinselWaitUntil(waitCond);
-
-      // Send handler
-      if (needToSend && tinselCanSend()) {
-        Dir d = neighbourList[edgesSent];
-        // Send both the source direction and the LSB of the time step
-        // in the first word of the message
-        edgeOut[d][0] = (opposite(d) << 1) | (t & 1);
-        tinselSend(neighbour[d], edgeOut[d]);
-        edgesSent++;
-      }
-
-      // Receive handler
+      // Receive edge temperatures
       if (tinselCanRecv()) {
-        volatile int* msg = tinselRecv();
-        Dir d = msg[0] >> 1;
-        // Is the received edge for the current or next time step?
-        if ((msg[0]&1) == (t&1)) {
-          edgeIn[d] = msg;
-          edgesReceived++; 
-        } else {
-          // Buffer the edge
-          edgeInBuffer[d] = msg;
+        volatile Msg* msg = tinselRecv();
+        if (msg->time == t) {
+          // For current time step
+          msgIn[msg->from] = msg;
+          received++;
+        }
+        else {
+          // For next time step
+          msgInBuffer[msg->from] = msg;
         }
       }
     }
 
-    // Switch buffers for next time step
-    gridPtr = subgrid;
-    subgrid = newSubgrid;
-    newSubgrid = gridPtr;
+    // Compute new temperatures
+    for (int y = 0; y < 8; y++) {
+      for (int x = 0; x < 8; x++) {
+        // Determine neighbourhood samples
+        int n = y == 0 ? msgIn[N]->temp[x] : subgrid[y-1][x];
+        int s = y == 7 ? msgIn[S]->temp[x] : subgrid[y+1][x];
+        int e = x == 7 ? msgIn[E]->temp[y] : subgrid[y][x+1];
+        int w = x == 0 ? msgIn[W]->temp[y] : subgrid[y][x-1];
+        // New temperature
+        newSubgrid[y][x] = subgrid[y][x] - (subgrid[y][x] - ((n+s+e+w) >> 2));
+        // Update output edges (to be sent to neighbours)
+        if (y == 0) msgOut[N]->temp[x] = newSubgrid[y][x];
+        if (y == 7) msgOut[S]->temp[x] = newSubgrid[y][x];
+        if (x == 7) msgOut[E]->temp[y] = newSubgrid[y][x];
+        if (x == 0) msgOut[W]->temp[y] = newSubgrid[y][x];
+      }
+    }
+
+    // Reallocate space for used messages
+    for (int i = 0; i < numNeighbours; i++) {
+      Dir d = neighbourList[i];
+      tinselAlloc(msgIn[d]);
+    }
+
+    // Recognise any buffered edges
+    received = 0;
+    for (int i = 0; i < 4; i++)
+      if (msgInBuffer[i] != 0) {
+        msgIn[i] = msgInBuffer[i];
+        msgInBuffer[i] = 0;
+        received++;
+      }
+
+    // Prepare for next iteration
+    subgridPtr = subgrid; subgrid = newSubgrid; newSubgrid = subgridPtr;
+    sent = 0;
   }
 
   // Finally, emit the state of the local subgrid
-  int x = (xPos << TinselLogWordsPerMsg) - xPos;
-  int y = (yPos << TinselLogWordsPerMsg) - yPos;
-  for (int i = 0; i < L; i++)
-    for (int j = 0; j < L; j++) {
+  int x = xPos * 8;
+  int y = yPos * 8;
+  for (int i = 0; i < 8; i++)
+    for (int j = 0; j < 8; j++) {
       // Transfer Y coord (12 bits), X coord (12 buts) and temp (8 bits)
       uint32_t coords = ((y+i) << 12) | (x+j);
       uint32_t temp = (subgrid[i][j] >> 16) & 0xff;
