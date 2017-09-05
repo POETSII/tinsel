ec493f4d44d56c6dbea67f2f1894064423603523
Matthew Naylor <mattfn@gmail.com>
2017-09-05 12:34:51 +0100

    Floating-point fused multiply-add instrs

--- tests/run.cpp
+++ tests/run.cpp
@@ -4,7 +4,7 @@
 
 const char* tests[] = {
   "fldst",    "fadd",   "fdiv",    "fcmp",
-  "fcvt_w",   "fcvt",
+  "fcvt_w",   "fcvt",   "fmadd",
   "addi",     "beq",    "bne",     "lh",
   "mulhsu",   "sb",     "slti",    "sra",
   "xori",     "add",    "bge",     "jalr",
