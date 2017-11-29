0c3b83ecc6257f1ae485295beab35088e64dcee6
Shane Fleming <sf306@ic.ac.uk>
2017-11-29 08:57:56 +0000

    merging the code for accessing the tinselCycleCount into softswitch_input

---
+++ include/config.h
@@ -0,0 +1,58 @@
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+#define TinselLogBytesPerLine 5
+#define TinselLogBytesPerDRAM 30
+#define TinselLogWordsPerMsg 4
+#define TinselLineSize 256
+#define TinselLogMaxCores 8
+#define TinselLogFlitsPerThread 6
+#define TinselLogMailboxesPerBoard 4
+#define TinselLogBytesPerBeat 5
+#define TinselLogCoresPerBoard 6
+#define TinselTargetBoard '"DE5"'
+#define TinselLogBeatsPerLine 0
+#define TinselWordsPerBeat 8
+#define TinselLogBeatsPerDRAM 25
+#define TinselLogWordsPerBeat 3
+#define TinselLogDRAMsPerBoard 1
+#define TinselLogTransmitBufferLen 2
+#define TinselDeviceFamily '"Stratix V"'
+#define TinselLogMsgsPerThread 4
+#define TinselLogThreadsPerCore 4
+#define TinselDRAMLatency 20
+#define TinselLogBytesPerFlit 4
+#define TinselMaxFlitsPerMsg 4
+#define TinselBeatWidth 256
+#define TinselLogCoresPerMailbox 2
+#define TinselLogThreadsPerBoard 10
+#define TinselLogWordsPerLine 3
+#define TinselBeatBurstWidth 1
+#define TinselLogInstrsPerCore 12
+#define TinselBeatsPerLine 1
+#define TinselLogMaxFlitsPerMsg 2
+#define TinselDCacheLogSetsPerThread 3
+#define TinselLogDCachesPerDRAM 3
+#define TinselWordsPerFlit 4
+#define TinselWordsPerLine 8
+#define TinselThreadsPerDRAM 512
+#define TinselCoresPerMailbox 4
+#define TinselMailboxesPerBoard 16
+#define TinselLogWordsPerFlit 2
+#define TinselLogBytesPerMsg 6
+#define TinselDCacheLogNumWays 2
+#define TinselBytesPerBeat 32
+#define TinselDCachesPerDRAM 8
+#define TinselDRAMLogMaxInFlight 4
+#define TinselMaxBootImageBytes 512
+#define TinselLogCoresPerDCache 2
+#define TinselCoresPerDCache 4
+#define TinselLogMaxThreads 12
+#define TinselLogThreadsPerDRAM 9
+#define TinselThreadsPerBoard 1024
+#define TinselDRAMsPerBoard 2
+#define TinselLogThreadsPerMailbox 6
+#define TinselLogLinesPerDRAM 25
+#define TinselLogScratchpadBytes 11
+
+#endif
