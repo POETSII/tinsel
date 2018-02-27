52ce5b68188e49fbc6ee05315307db3b897bd090
Matthew Naylor <mn416@aesop.cl.cam.ac.uk>
2018-02-27 16:31:50 +0000

    Cleaner version of pciestreamd
    Also call powerup() and powerdown() in HostLink constructor and
    destructor where appropriate

--- hostlink/pciestreamd.c
+++ hostlink/pciestreamd.c
@@ -3,7 +3,7 @@
 // PCIeStream Daemon
 // =================
 //
-// Connect UNIX domain sockets to FPGA FIFOs via PCIeStream.
+// Connect UNIX domain socket to FPGA FIFO via PCIeStream.
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,14 +19,13 @@
 #include <stdint.h>
 #include <assert.h>
 #include <poll.h>
+#include <errno.h>
 
 // Constants
 // ---------
 
-// Default socket locations
-#define SOCKET_IN   "pciestream-in"
-#define SOCKET_OUT  "pciestream-out"
-#define SOCKET_CTRL "pciestream-ctrl"
+// Default socket location
+#define SOCKET_NAME "pciestream"
 
 // Size of each DMA buffer in bytes
 #define DMABufferSize 1048576
@@ -74,281 +73,199 @@ void swap(volatile char** p, volatile char** q)
   volatile char* tmp = *p; *p = *q; *q = tmp;
 }
 
-// Transmitter thread
-// ------------------
+// Check if connection is alive
+int alive(int sock)
+{
+  //char buf;
+  //int ret = recv(sock, &buf, 1, MSG_PEEK | MSG_DONTWAIT);
+  //int closed = ret == 0 || (ret == -1 && errno != EAGAIN);
+  char buf;
+  return send(sock, &buf, 0, 0) == 0;
+}
+
+// Types
+// -----
+
+// Return value of transmitter or receiver step function
+typedef enum {
+  CLOSED,      // Client has closed the connection
+  FULL,        // Transmit buffer is full
+  EMPTY,       // Receive buffer is empty
+  NO_SEND,     // Nothing to send from client
+  CLIENT_BUSY, // Client can't currently receive more data
+  PROGRESS     // Progress was made
+} Status;
+
+// Transmitter state
+typedef struct {
+  // Access to control/status registers on FPGA side
+  volatile uint64_t* csrs;
+  // Transmit buffer A
+  volatile char* txA;
+  // Transmit buffer B
+  volatile char* txB;
+  // Client connection (socket)
+  int client;
+  // Which buffer in the double buffer is currently being written to?
+  int activeBuffer;
+  // Is the active buffer ready to be filled?
+  int bufferReady;
+  // The number of bytes written to the active buffer but not yet sent
+  int pending;
+} TxState;
+
+// Receiver state
+typedef struct {
+  // Access to control/status registers on FPGA side
+  volatile uint64_t* csrs;
+  // Receive buffer A
+  volatile char* rxA;
+  // Receive buffer B
+  volatile char* rxB;
+  // Client connection (socket)
+  int client;
+  // Which buffer in the double buffer is currently being read from?
+  int activeBuffer;
+  // Number of bytes written to the client
+  int written;
+  // The number of bytes in the DMA buffer available for reading
+  int available;
+} RxState;
+
+// Transmitter
+// -----------
+
+// Intialise the transmitter state
+void txInit(TxState* s, int client, volatile uint64_t* csrs,
+       volatile char* txA, volatile char* txB)
+{
+  s->client = client;
+  s->csrs = csrs;
+  s->txA = txA;
+  s->txB = txB;
+  s->activeBuffer = 0;
+  s->bufferReady = 0;
+  s->pending = 0;
+}
 
 // Read from socket and write to FPGA
-void transmitter(
-       volatile uint64_t* csrs,
-         volatile char* txA,
-           volatile char* txB)
+Status tx(TxState* s)
 {
-  // Create socket
-  int sock = socket(AF_UNIX, SOCK_STREAM, 0);
-  if (sock == -1) {
-    perror("Transmitter: socket");
-    exit(EXIT_FAILURE);
+  // This flag indicates that data should now be sent
+  int doSend = 0;
+
+  // Send pending data if buffer is full
+  if (s->pending == DMABufferSize) doSend = 1;
+
+  // Send pending data if: (1) pending is a non-zero multiple of
+  // 16 and (2) there's no data available on the socket.
+  if (s->pending != 0 && (s->pending&0xf) == 0) {
+    struct pollfd fd; fd.fd = s->client; fd.events = POLLIN;
+    int ret = poll(&fd, 1, 0);
+    if (ret <= 0) doSend = 1;
   }
 
-  // Bind socket
-  struct sockaddr_un sockAddr;
-  memset(&sockAddr, 0, sizeof(struct sockaddr_un));
-  sockAddr.sun_family = AF_UNIX;
-  sockAddr.sun_path[0] = '\0';
-  strncpy(&sockAddr.sun_path[1], SOCKET_IN, sizeof(sockAddr.sun_path)-2);
-  int ret = bind(sock, (const struct sockaddr *) &sockAddr,
-                   sizeof(struct sockaddr_un));
-  if (ret == -1) {
-    perror("Transmitter: bind");
-    exit(EXIT_FAILURE);
-  }
-
-  // Listen for connections
-  ret = listen(sock, 0);
-  if (ret == -1) {
-    perror("Transmitter: listen");
-    exit(EXIT_FAILURE);
-  }
-
-  // Which buffer in the double buffer is currently being written to?
-  int activeBuffer = 0;
-
-  // Is the DMA buffer ready to be filled
-  int bufferReady = 0;
-
-  for (;;) {
-    // Accept connection
-    int conn = accept(sock, NULL, NULL);
-    if (conn == -1) {
-      perror("Transmitter: accept");
-      exit(EXIT_FAILURE);
+  // Try to read data from client
+  if (! doSend) {
+    // Determine if DMA buffer is available
+    if (! s->bufferReady) {
+      if (s->csrs[2*(CSR_LEN_TX_A + s->activeBuffer)] == 0)
+        s->bufferReady = 1;
+      else
+        return FULL;
     }
-
-    // The number of bytes written to DMA buffer but not yet sent
-    int pending = 0;
-
-    int restart = 0;
-    while (! restart) {
-      // This flag indicates that data should now be sent
-      int doSend = 0;
-
-      // Send pending data if buffer is full
-      if (pending == DMABufferSize) doSend = 1;
-
-      // Send pending data if: (1) pending is a non-zero multiple of
-      // 16 and (2) there's no data available on the socket.
-      if (pending != 0 && (pending&0xf) == 0) {
-        struct pollfd fd; fd.fd = conn; fd.events = POLLIN;
-        int ret = poll(&fd, 1, 1);
-        if (ret <= 0) doSend = 1;
-      }
-
-      // Try to read data
-      if (! doSend) {
-        // Wait until DMA buffer available
-        if (! bufferReady) {
-          while (csrs[2*(CSR_LEN_TX_A+activeBuffer)] != 0) usleep(100);
-          bufferReady = 1;
-        }
-        // Read data
-        int n = read(conn, (void*) &txA[pending], DMABufferSize-pending);
-        if (n <= 0) {
-          // On EOF, send pending data and restart
-          if (pending >= 16) doSend = 1;
-          close(conn);
-          restart = 1;
-        }
-        else {
-          pending += n;
-        }
-      }
-
-      // Send pending data, if requested
-      if (doSend) {
-        // Flush cache
-        mfence();
-        for (int i = 0; i < pending; i += CacheLineBytes) clflush(&txA[i]);
-        mfence();
-        // Trigger send
-        assert(bufferReady && pending >= 16);
-        csrs[2*(CSR_LEN_TX_A+activeBuffer)] = pending/16;
-        // Switch buffers
-        swap(&txA, &txB);
-        activeBuffer = (activeBuffer+1)&1;
-        pending = 0;
-        bufferReady = 0;
-      }
+    // Is there any data to transmit?
+    struct pollfd fd; fd.fd = s->client; fd.events = POLLIN;
+    int ret = poll(&fd, 1, 0);
+    if (ret == 0)
+      return NO_SEND;
+    else if (ret < 0)
+      return CLOSED;
+    else {
+      // Read data from client
+      int n = read(s->client, (void*) &s->txA[s->pending],
+                DMABufferSize - s->pending);
+      if (n <= 0) return CLOSED;
+      s->pending += n;
     }
   }
+
+  // Send pending data, if requested
+  if (doSend) {
+    // Flush cache
+    mfence();
+    for (int i = 0; i < s->pending; i += CacheLineBytes) clflush(&s->txA[i]);
+    mfence();
+    // Trigger send
+    assert(s->bufferReady && s->pending >= 16);
+    s->csrs[2*(CSR_LEN_TX_A + s->activeBuffer)] = s->pending/16;
+    // Switch buffers
+    swap(&s->txA, &s->txB);
+    s->activeBuffer = (s->activeBuffer+1)&1;
+    s->pending = 0;
+    s->bufferReady = 0;
+  }
+
+  return PROGRESS;
 }
 
-// Receiver thread
-// ---------------
+// Receiver
+// --------
+
+// Initialise the receiver state
+void rxInit(RxState* s, int client, volatile uint64_t* csrs,
+       volatile char* rxA, volatile char* rxB)
+{
+  s->client = client;
+  s->csrs = csrs;
+  s->rxA = rxA;
+  s->rxB = rxB;
+  s->activeBuffer = 0;
+  s->written = 0;
+  s->available = 0;
+}
 
 // Read from FPGA and write to socket
-void receiver(
-       volatile uint64_t* csrs,
-         volatile char* rxA,
-           volatile char* rxB)
+Status rx(RxState* s) 
 {
-
-  // Create socket
-  int sock = socket(AF_UNIX, SOCK_STREAM, 0);
-  if (sock == -1) {
-    perror("Receiver: socket");
-    exit(EXIT_FAILURE);
+  // Determine if data is available to receive
+  if (s->available == 0) {
+    s->available = 16 * s->csrs[2*(CSR_LEN_RX_A + s->activeBuffer)];
+    if (s->available == 0)
+      return EMPTY;
+    // Cache flush
+    for (int i = 0; i < s->available; i += CacheLineBytes) clflush(&s->rxA[i]);
+    mfence();
   }
 
-  // Bind socket
-  struct sockaddr_un sockAddr;
-  memset(&sockAddr, 0, sizeof(struct sockaddr_un));
-  sockAddr.sun_family = AF_UNIX;
-  sockAddr.sun_path[0] = '\0';
-  strncpy(&sockAddr.sun_path[1], SOCKET_OUT, sizeof(sockAddr.sun_path)-2);
-  int ret = bind(sock, (const struct sockaddr *) &sockAddr,
-                   sizeof(struct sockaddr_un));
-  if (ret == -1) {
-    perror("Receiver: bind");
-    exit(EXIT_FAILURE);
-  }
+  // Can we send data to the client?
+  struct pollfd fd; fd.fd = s->client; fd.events = POLLOUT;
+  int ret = poll(&fd, 1, 0);
+  if (ret == 0)
+    return CLIENT_BUSY;
+  else if (ret < 0)
+    return CLOSED;
+  else {
+    // Write data to socket
+    int n = write(s->client, (void*) &s->rxA[s->written],
+              s->available - s->written);
+    if (n <= 0) return CLOSED;
+    s->written += n;
 
-  // Listen for connections
-  ret = listen(sock, 0);
-  if (ret == -1) {
-    perror("Receiver: listen");
-    exit(EXIT_FAILURE);
-  }
-
-  // Which buffer in the double buffer is currently being read from?
-  int activeBuffer = 0;
-
-  // Number of bytes written to the socket
-  int written = 0;
-
-  for (;;) {
-    // Accept connection
-    int conn = accept(sock, NULL, NULL);
-    if (conn == -1) {
-      perror("Receiver: accept");
-      exit(EXIT_FAILURE);
-    }
-
-    // The number of bytes in the DMA buffer available for reading
-    int available = 0;
-
-    for (;;) {
-      // Wait until data available
-      if (available == 0) {
-        for (;;) {
-          available = 16 * csrs[2*(CSR_LEN_RX_A+activeBuffer)];
-          if (available != 0) break;
-        }
-        // Cache flush
-        for (int i = 0; i < available; i += CacheLineBytes) clflush(&rxA[i]);
-        mfence();
-      }
- 
-      // Write data to socket
-      int n = write(conn, (void*) &rxA[written], available-written);
-      if (n <= 0) {
-        close(conn);
-        break;
-      }
-      else {
-        written += n;
-      }
-
-      // Consume data from FPGA
-      if (written == available) {
-        // Make sure all the reads are done before the next write
-        mfence();
-        // Finished with this buffer
-        csrs[2*(CSR_LEN_RX_A+activeBuffer)] = 0;
-        // Switch buffers
-        swap(&rxA, &rxB);
-        activeBuffer = (activeBuffer+1)&1;
-        available = written = 0;
-      }
-    }
-  }
-}
-
-// Control thread
-// --------------
-
-void control(pid_t pidTransmitter, pid_t pidReceiver)
-{
-  // Static variables
-  static int sock = -1;
-  static int conn = -1;
-
-  if (sock == -1) {
-    // Create socket
-    sock = socket(AF_UNIX, SOCK_STREAM, 0);
-    if (sock == -1) {
-      perror("Control: socket");
-      exit(EXIT_FAILURE);
-    }
-
-    // Bind socket
-    struct sockaddr_un sockAddr;
-    memset(&sockAddr, 0, sizeof(struct sockaddr_un));
-    sockAddr.sun_family = AF_UNIX;
-    sockAddr.sun_path[0] = '\0';
-    strncpy(&sockAddr.sun_path[1], SOCKET_CTRL,
-      sizeof(sockAddr.sun_path)-2);
-    int ret = bind(sock, (const struct sockaddr *) &sockAddr,
-                     sizeof(struct sockaddr_un));
-    if (ret == -1) {
-      perror("Control: bind");
-      exit(EXIT_FAILURE);
-    }
-
-    // Listen for connections
-    ret = listen(sock, 0);
-    if (ret == -1) {
-      perror("Control: listen");
-      exit(EXIT_FAILURE);
+    // Consume data from FPGA
+    if (s->written == s->available) {
+      // Make sure all the reads are done before the next write
+      mfence();
+      // Finished with this buffer
+      s->csrs[2*(CSR_LEN_RX_A + s->activeBuffer)] = 0;
+      // Switch buffers
+      swap(&s->rxA, &s->rxB);
+      s->activeBuffer = (s->activeBuffer+1)&1;
+      s->available = s->written = 0;
     }
   }
 
-  for (;;) {
-    if (conn == -1) {
-      // Accept connection
-      conn = accept(sock, NULL, NULL);
-      if (conn == -1) {
-        perror("Control: accept");
-        exit(EXIT_FAILURE);
-      }
-    }
-
-    for (;;) {
-      char cmd;
-      int n = read(conn, &cmd, 1);
-      if (n <= 0) {
-        close(conn);
-        conn = -1;
-        break;
-      }
-      else {
-        // Kill threads on reset and exit commands
-        if (cmd == 'r' || cmd == 'e') {
-          kill(pidTransmitter, SIGTERM);
-          kill(pidReceiver, SIGTERM);
-          int status;
-          waitpid(pidTransmitter, &status, 0);
-          waitpid(pidReceiver, &status, 0);
-        }
-        if (cmd == 'r') return;
-        else if (cmd == 'e') exit(EXIT_SUCCESS);
-        else if (cmd == 'p') {
-          // Respond to ping command
-          char resp = 'p';
-          int n = write(conn, &resp, 1);
-        }
-      }
-    }
-  }
+  return PROGRESS;
 }
 
 // Main function
@@ -391,6 +308,40 @@ volatile char* openDMABuffer(char* deviceFile, int prot, uint64_t* addr)
   }
 
   return (volatile char*) ptr;
+}
+
+// Create listening socket
+int createListener()
+{
+  // Create socket
+  int sock = socket(AF_UNIX, SOCK_STREAM, 0);
+  if (sock == -1) {
+    perror("pciestreamd: socket");
+    exit(EXIT_FAILURE);
+  }
+
+  // Bind socket
+  struct sockaddr_un sockAddr;
+  memset(&sockAddr, 0, sizeof(struct sockaddr_un));
+  sockAddr.sun_family = AF_UNIX;
+  sockAddr.sun_path[0] = '\0';
+  strncpy(&sockAddr.sun_path[1], SOCKET_NAME,
+    sizeof(sockAddr.sun_path)-2);
+  int ret = bind(sock, (const struct sockaddr *) &sockAddr,
+                   sizeof(struct sockaddr_un));
+  if (ret == -1) {
+    perror("pciestreamd: bind");
+    exit(EXIT_FAILURE);
+  }
+
+  // Listen for connections
+  ret = listen(sock, 0);
+  if (ret == -1) {
+    perror("Control: listen");
+    exit(EXIT_FAILURE);
+  }
+
+  return sock;
 }
 
 int main(int argc, char* argv[])
@@ -441,7 +392,21 @@ int main(int argc, char* argv[])
   // Main loop
   // ---------
 
+  // Create listener socket
+  int sock = createListener();
+
+  // Transmitter and receiver state
+  TxState txState;
+  RxState rxState;
+
   for (;;) {
+    // Accept connection
+    int conn = accept(sock, NULL, NULL);
+    if (conn == -1) {
+      perror("pciestreamd: accept");
+      exit(EXIT_FAILURE);
+    }
+
     // Reset PCIeStream hardware
     csrs[2*CSR_EN] = 0;
     while (csrs[2*CSR_INFLIGHT] != 0);
@@ -453,16 +418,23 @@ int main(int argc, char* argv[])
     csrs[2*CSR_ADDR_TX_B] = addrTxB;
     csrs[2*CSR_EN] = 1;
 
-    // Transmitter thread
-    pid_t pidTransmitter = fork();
-    if (pidTransmitter == 0) transmitter(csrs, txA, txB);
+    // Reset state
+    txInit(&txState, conn, csrs, txA, txB);
+    rxInit(&rxState, conn, csrs, rxA, rxB);
 
-    // Receiver thread
-    pid_t pidReceiver = fork();
-    if (pidReceiver == 0) receiver(csrs, rxA, rxB);
+    // Event loop
+    for (;;) {
+      Status txStatus = tx(&txState);
+      if (txStatus == CLOSED) break;
+      Status rxStatus = rx(&rxState);
+      if (rxStatus == CLOSED) break;
+      if (txStatus != PROGRESS && rxStatus != PROGRESS) {
+        if (! alive(conn)) break;
+        usleep(100);
+      }
+    }
 
-    // Control thread
-    control(pidTransmitter, pidReceiver);
+    close(conn);
   }
 
   return 0;
