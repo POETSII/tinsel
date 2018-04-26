a8539ed66e7ee6c7077436c9770790d0604ba855
Matthew Naylor <mn416@cl.cam.ac.uk>
2018-04-26 21:54:09 +0100

    Assert SRAM reset on startup

--- de5/Golden_top.v
+++ de5/Golden_top.v
@@ -233,6 +233,8 @@ wire rst_50mhz_n = 1;
 wire clk_156mhz;
 wire phy_pll_locked;
 
+reg rst_sram_n = 0;
+
 wire ddr3_local_init_done;
 wire ddr3_local_cal_success;
 wire ddr3_2_local_init_done;
@@ -515,8 +517,8 @@ S5_DDR3_QSYS u0 (
 
   .clk_a_b_c_clk(OSC_50_B4A),
   .clk_d_clk(OSC_50_B8D),
-  .reset_a_b_c_reset_n(rst_50mhz_n),
-  .reset_d_reset_n(rst_50mhz_n),
+  .reset_a_b_c_reset_n(rst_sram_n),
+  .reset_d_reset_n(rst_sram_n),
 
   .oct_0_rzqin(RZQ_1),
 
@@ -593,5 +595,12 @@ temp_display temp_display_inst (
   .HEX1_D(HEX1_D),
   .HEX1_DP(HEX1_DP)
 );
+
+// Reset SRAMs
+reg [7:0] rst_sram_count = 0;
+always @(posedge clk_50mhz) begin
+  if (rst_sram_count == 255) rst_sram_n <= 1;
+  else rst_sram_count <= rst_sram_count + 1;
+end
 
 endmodule 
