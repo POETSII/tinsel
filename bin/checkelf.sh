b643625be585b5f835983bcbce5439557f5060ed
Matthew Naylor <mattfn@gmail.com>
2018-05-16 09:54:27 +0100

    Drop support for fused multiply-add

--- bin/checkelf.sh
+++ bin/checkelf.sh
@@ -6,13 +6,14 @@ if [ -z "$1" ]; then
   echo "Usage: checkelf.sh <FILE>"
 fi
 
-DUMP=$(riscv64-unknown-elf-objdump -S $1)
+DUMP=$(riscv64-unknown-elf-objdump -d $1)
 
 # Errors
-ES="ecall|ebreak"
-ES="$ES|csrrs|csrrc|csrrwi|csrrsi|csrrci"
-ES="$ES|[^f]div|divu|rem|remu"
-ES="$ES|fsqrt|fmin|fmax|fclassify"
+ES="\secall\s|ebreak\s"
+ES="$ES|\scsrrs\s|csrrc\s|\scsrrwi\s|\scsrrsi\s|\scsrrci\s"
+ES="$ES|\s[^f]div\s|divu\s|\srem\s|\sremu\s"
+ES="$ES|\sfsqrt\s|fmin\s|fmax\s|\sfclassify\s"
+ES="$ES|\sfmadd\s|fmsub\s|fnmadd\s|\sfnmsub\s"
 
 if echo "$DUMP" | grep -q -E "$ES"; then
   echo "ERROR: $1 uses unsupported instructions:"
