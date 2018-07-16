fa53db3ae8b36c19b47aba49a9f8703f2b168bd7
mn416 <mn416@zitura.cl.cam.ac.uk>
2018-07-16 20:25:51 +0100

    Generalise ring oscillator

---
+++ apps/ring/ring.h
@@ -0,0 +1,10 @@
+#ifndef _RING_H_
+#define _RING_H_
+
+#define NUM_BOARDS            1
+#define LOG_THREADS_PER_BOARD 10
+#define NUM_TOKENS            1000
+#define NUM_LOOPS             10000
+#define RANDOM                0
+
+#endif
