0f25b0f345839dde955b092ccd57b2982fb642f8
Matthew Naylor <mn416@aesop.cl.cam.ac.uk>
2018-07-16 20:40:03 +0100

    Tweaks to ring oscillator

--- apps/ring/GenLayout.cpp
+++ apps/ring/GenLayout.cpp
@@ -27,8 +27,8 @@ int main()
     // Generate and shuffle
     for (int i = 0; i < numThreads; i++) seq[i] = i;
     srand(0);
-    for (int i = 1; i < numThreads-1; i++) {
-      int j = i + rand() % (numThreads-i);
+    for (int i = 1; i < numThreadsUsed-1; i++) {
+      int j = i + rand() % (numThreadsUsed-i);
       swap(seq[i], seq[j]);
     }
 
@@ -45,14 +45,19 @@ int main()
     uint32_t threadIncr = 1 << (TinselLogThreadsPerBoard-LOG_THREADS_PER_BOARD);
 
     for (int i = 0; i < numThreads; i++) {
-      // Next thread in ring
-      uint32_t next = (i+threadIncr) >= numThreads ? 0 :
-        i+(1 << (TinselLogThreadsPerBoard - LOG_THREADS_PER_BOARD));
-
-      if ((i & (threadIncr-1)) == 0)
-        neighbour[i] = next;
-      else
+      if (i >= numThreadsUsed) {
         neighbour[i] = -1;
+      }
+      else {
+        // Next thread in ring
+        uint32_t next = (i+threadIncr) >= numThreadsUsed ? 0 :
+          i+(1 << (TinselLogThreadsPerBoard - LOG_THREADS_PER_BOARD));
+
+        if ((i & (threadIncr-1)) == 0)
+          neighbour[i] = next;
+        else
+          neighbour[i] = -1;
+      }
     }
   }
 
