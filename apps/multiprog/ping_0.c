accc09f551520f9a9f1b2ea6b01033684d3dc0d4
mn416 <mn416@zitura.cl.cam.ac.uk>
2018-07-23 13:47:03 +0100

    Tweaks to multiprog app

--- apps/multiprog/ping_0.c
+++ apps/multiprog/ping_0.c
@@ -6,9 +6,6 @@ uint32_t incVal = 1;
 
 int main()
 {
-  // Get thread id
-  int me = tinselId();
-
   // Get host id
   int host = tinselHostId();
 
@@ -16,14 +13,12 @@ int main()
   volatile int* msgIn = tinselSlot(0);
   volatile int* msgOut = tinselSlot(1);
 
-  while (me == 0) {
-    tinselAlloc(msgIn);
-    tinselWaitUntil(TINSEL_CAN_RECV);
-    tinselRecv();
-    tinselWaitUntil(TINSEL_CAN_SEND);
-    msgOut[0] = msgIn[0]+incVal;
-    tinselSend(host, msgOut);
-  }
+  tinselAlloc(msgIn);
+  tinselWaitUntil(TINSEL_CAN_RECV);
+  tinselRecv();
+  tinselWaitUntil(TINSEL_CAN_SEND);
+  msgOut[0] = msgIn[0]+incVal;
+  tinselSend(host, msgOut);
 
   return 0;
 }
