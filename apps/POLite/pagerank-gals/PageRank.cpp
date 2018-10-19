dc554f796fbc1abde90a9b301784377cec2eb523
Matthew Naylor <mn416@byron.cl.cam.ac.uk>
2018-10-19 16:52:27 +0000

    Merged in GALS PageRank -- thanks Shane!

---
+++ apps/POLite/pagerank-gals/PageRank.cpp
@@ -0,0 +1,23 @@
+#include "PageRank.h"
+
+#include <tinsel.h>
+#include <POLite.h>
+
+typedef PThread<
+          PageRankDevice,
+          None,             // Accumulator (small state)
+          PageRankState,    // State
+          None,             // Edge label
+          PageRankMessage   // Message
+        > PageRankThread;
+
+int main()
+{
+  // Point thread structure at base of thread's heap
+  PageRankThread* thread = (PageRankThread*) tinselHeapBaseSRAM();
+  
+  // Invoke interpreter
+  thread->run();
+
+  return 0;
+}
