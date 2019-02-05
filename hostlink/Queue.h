86cded74c0b77fbfb0ee8e96fd97d0a47c4edf3f
Matthew Naylor <mattfn@gmail.com>
2019-02-05 14:57:47 +0000

    Finish boardctrld (untested)

---
+++ hostlink/Queue.h
@@ -0,0 +1,61 @@
+// Simple queue implementation (circular buffer)
+
+#ifndef _QUEUE_H_
+#define _QUEUE_H_
+
+template <typename T> struct Queue {
+  int capacity;
+  int front, back;
+  int size;
+  T* data;
+
+  Queue(int n) {
+    capacity = n+1;
+    data = new T [capacity];
+    size = 0;
+  }
+
+  inline T first() {
+    return data[front];
+  }
+
+  inline T index(int i) {
+    return data[(front+i) % capacity];
+  }
+
+  inline void enq(T elem) {
+    data[back] = elem;
+    back++;
+    size++;
+  }
+
+  inline T deq() {
+    T elem = data[front];
+    front++;
+    size--;
+    return elem;
+  }
+
+  inline void drop(int n) {
+    front = (front+n) % capacity;
+    size = size - n;
+  }
+
+  inline int space() {
+    return (capacity-1-size);
+  }
+
+  inline bool canEnq() {
+    return space() > 0;
+  }
+
+  inline bool canDeq() {
+    return size > 0;
+  }
+
+  ~Queue() {
+    delete [] data;
+  }
+};
+
+#endif
