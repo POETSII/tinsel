f3c53592260e49d7dc1faf4de3274b1d45b81e60
Matthew Naylor <mattfn@gmail.com>
2019-05-09 09:06:02 +0100

    Better control over bridge board

--- hostlink/DebugLinkFormat.h
+++ hostlink/DebugLinkFormat.h
@@ -9,6 +9,7 @@
 #define DEBUGLINK_SET_DEST  1
 #define DEBUGLINK_STD_IN    2
 #define DEBUGLINK_STD_OUT   2
+#define DEBUGLINK_EN_IDLE   3
 #define DEBUGLINK_READY     255
 
 // Maximum size of a DebugLink packet
@@ -19,6 +20,7 @@ inline int toDebugLinkSize(uint8_t cmd)
 {
   switch (cmd) {
     case DEBUGLINK_QUERY_IN: return 3;
+    case DEBUGLINK_EN_IDLE: return 3;
     case DEBUGLINK_SET_DEST: return 3;
     case DEBUGLINK_STD_IN: return 2;
     default:
