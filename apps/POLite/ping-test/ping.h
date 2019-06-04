e4437cf88d9af1d896cb499ed48def71adc0464f
Jonny Beaumont <jonathan.r.beaumont@gmail.com>
2019-06-04 15:27:06 +0000

    Added host-device messaging POLite test app

---
+++ apps/POLite/ping-test/ping.h
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: BSD-2-Clause
+// Test messaging between host and threads.
+
+#ifndef _ping_H_
+#define _ping_H_
+
+//#define POLITE_DUMP_STATS
+//#define POLITE_COUNT_MSGS
+
+// Lightweight POETS frontend
+#include <POLite.h>
+
+struct PingMessage {
+  // Number to confirm message being transferred is not corrupted
+  uint32_t test;
+};
+
+struct PingState {
+  // Number received to be sent back to host
+  uint32_t test;
+};
+
+struct PingDevice : PDevice<PingState, None, PingMessage> {
+  // Called once by POLite at start of execution
+  void init() {
+    // Do nothing until a message is received from the host
+    *readyToSend = No;
+  }
+
+  // Send handler
+  inline void send(volatile PingMessage* msg) {
+    // Put received value back in message (+1) for host to check
+    msg->test = s->test;
+  }
+
+  // Receive handler
+  inline void recv(PingMessage* msg, None* edge) {
+    // Store number from host to send back to host
+    s->test = msg->test;
+    *readyToSend = HostPin;
+  }
+
+  // Called by POLite when system becomes idle
+  inline bool step() {
+    *readyToSend = No;
+    return true;
+  }
+
+  // Optionally send message to host on termination
+  inline bool finish(volatile PingMessage* msg) {
+    return true;
+  }
+};
+
+#endif
