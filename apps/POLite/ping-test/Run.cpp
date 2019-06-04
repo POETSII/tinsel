45020212c431fdd4c8454812547bfd608a80e8a9
Jonny Beaumont <jonathan.r.beaumont@gmail.com>
2019-06-04 15:27:06 +0000

    Added host-device messaging POLite test app

---
+++ apps/POLite/ping-test/Run.cpp
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: BSD-2-Clause
+#include "ping.h"
+
+#include <HostLink.h>
+#include <POLite.h>
+#include <EdgeList.h>
+#include <assert.h>
+#include <sys/time.h>
+#include <config.h>
+
+int main(int argc, char**argv)
+{
+  // Connection to tinsel machine
+  HostLink hostLink;
+
+  // Create POETS graph
+  PGraph<PingDevice, PingState, None, PingMessage> graph;
+
+  // Create single ping device
+  PDeviceId id = graph.newDevice();
+
+  // Prepare mapping from graph to hardware
+  graph.map();
+
+  // Write graph down to tinsel machine via HostLink
+  graph.write(&hostLink);
+
+  // Load code and trigger execution
+  hostLink.boot("code.v", "data.v");
+  hostLink.go();
+
+  // Timer
+  printf("Ping started\n");
+
+  // Consume performance stats
+  politeSaveStats(&hostLink, "stats.txt");
+
+  int test = 0;
+  int deviceAddr = graph.toDeviceAddr[id];
+  printf("deviceAddr = %d\n", deviceAddr);
+  while (test < 100) {
+    PMessage<None, PingMessage> sendMsg;
+    printf("Size of msg = %lu\n", sizeof(sendMsg));
+    sendMsg.payload.test = test;
+    hostLink.send(deviceAddr, 1, &sendMsg);
+    printf("Sent %d to device\n", sendMsg.payload.test);
+
+
+    while (!(hostLink.canRecv())) {};
+    PMessage<None, PingMessage> recvMsg;
+    hostLink.recvMsg(&recvMsg, sizeof(recvMsg));
+    printf("Received %d from device\n", recvMsg.payload.test);
+
+    test++;
+  }
+
+  return 0;
+}
