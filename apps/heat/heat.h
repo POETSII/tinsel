6ac0690ebfb691528dd190a9d1019bb27c6b00df
Matthew Naylor <mn416@heaney.cl.cam.ac.uk>
2019-07-05 09:18:17 +0000

    Forgot file in previous commit

--- apps/heat/heat.h
+++ apps/heat/heat.h
@@ -4,19 +4,9 @@
 #ifndef _HEAT_H_
 #define _HEAT_H_
 
-// Each board uses a X_LOCAL_LEN x Y_LOCAL_LEN grid of threads
-#define X_LOCAL_LEN (1 << ((TinselLogThreadsPerBoard+1) >> 1))
-#define Y_LOCAL_LEN (1 << (TinselLogThreadsPerBoard >> 1))
-
-// And there is a 2D mesh of boards
-// Assumption 1: X_BOARDS <= TinselMeshXLenWithinBox
-// Assumption 2: Y_BOARDS <= TinselMeshYLenWithinBox
-#define X_BOARDS 1
-#define Y_BOARDS 1
-
-// In all, the program uses an X_LEN * Y_LEN grid of threads
-#define X_LEN (X_LOCAL_LEN*X_BOARDS)
-#define Y_LEN (Y_LOCAL_LEN*Y_BOARDS)
+// Number of POETS boxes to use
+#define X_BOXES 1
+#define Y_BOXES 1
 
 // Format of messages sent to host
 typedef struct {
