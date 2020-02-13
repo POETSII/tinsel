43a2874b2b430448ad2e185de285e954fd1c2a85
Matthew Naylor <mn416@jennings.cl.cam.ac.uk>
2020-02-13 14:11:08 +0000

    New POLite example: Izhikevich

---
+++ apps/POLite/izhikevich-pc/RNG.h
@@ -0,0 +1,27 @@
+#ifndef _RNG_H_
+#define _RNG_H_
+
+inline uint32_t urng(uint32_t &state) {
+  state = state*1664525+1013904223;
+  return state;
+}
+
+// World's crappiest gaussian (courtesy of dt10!)
+inline float grng(uint32_t &state) {
+  uint32_t u=urng(state);
+  int32_t acc=0;
+  for(unsigned i=0;i<8;i++){
+    acc += u&0xf;
+    u=u>>4;
+  }
+  // a four-bit uniform has mean 7.5 and variance ((15-0+1)^2-1)/12 = 85/4
+  // sum of four uniforms has mean 8*7.5=60 and variance of 8*85/4=170
+  const float scale=0.07669649888473704; // == 1/sqrt(170)
+  return (acc-60.0f) * scale;
+}
+
+inline double urand() {
+  return (double) rand() / RAND_MAX;
+}
+
+#endif
---
+++ apps/POLite/izhikevich-sync/Izhikevich.h
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: BSD-2-Clause
+// (Based on code by David Thomas)
+#ifndef _Izhikevich_H_
+#define _Izhikevich_H_
+
+#define POLITE_DUMP_STATS
+#define POLITE_COUNT_MSGS
+
+#include <POLite.h>
+#include "RNG.h"
+
+// Number of time steps to run for
+#define NUM_STEPS 1000
+
+// Vertex state
+struct IzhikevichState {
+  // Random-number-generator state
+  uint32_t rng;
+  // Neuron state
+  float u, v, I, spikeCount;
+  // Neuron properties
+  float a, b, c, d, Ir;
+};
+
+// Edge weight type
+typedef float Weight;
+
+// Message type
+struct IzhikevichMsg {
+  // Number of times sender has spiked
+  uint32_t spikeCount;
+};
+
+// Vertex behaviour
+struct IzhikevichDevice : PDevice<IzhikevichState,Weight,IzhikevichMsg> {
+  inline void init() {
+    s->v = -65.0f;
+    s->u = s->b * s->v;
+    s->I = s->Ir * grng(s->rng);
+    *readyToSend = No;
+  }
+  inline void send(IzhikevichMsg* msg) {
+    s->spikeCount++;
+    msg->spikeCount = s->spikeCount;
+    *readyToSend = No;
+  }
+  inline void recv(IzhikevichMsg* msg, Weight* weight) {
+    s->I += *weight;
+  }
+  inline bool step() {
+    float &v = s->v;
+    float &u = s->u;
+    float &I = s->I;
+    v = v+0.5*(0.04*v*v+5*v+140-u+I); // Step 0.5 ms
+    v = v+0.5*(0.04*v*v+5*v+140-u+I); // for numerical
+    u = u + s->a*(s->b*v-u);          // stability
+    if (v >= 30.0) {
+      v = s->c;
+      u += s->d;
+      *readyToSend = Pin(0);
+    }
+    s->I = s->Ir * grng(s->rng);
+    return (time < NUM_STEPS);
+  }
+  inline bool finish(IzhikevichMsg* msg) {
+    msg->spikeCount = s->spikeCount;
+    return true;
+  }
+};
+
+#endif
