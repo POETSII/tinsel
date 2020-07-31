58429bb53006826f12ed8ba1a839fe5cd5016c0d
jordmorr <jordan.morris@newcastle.ac.uk>
2020-07-31 18:42:05 +0100

    Model Added to tinsel level imputation app

---
+++ apps/imputation/model.h
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: BSD-2-Clause
+#ifndef _MODEL_H_
+#define _MODEL_H_
+
+#include <stdint.h>
+
+// Parameters
+
+/***************************************************
+ * Edit values between here ->
+ * ************************************************/
+
+#define NOOFSTATES (128)
+#define NOOFOBS (24)
+#define NOOFTARGMARK (4)
+#define NE (1000000)
+#define ERRORRATE (10000)
+
+// Pre-processor Switches
+//#define PRINTDIAG (1)
+
+/***************************************************
+ * <- And here
+ * ************************************************/
+
+extern const uint32_t observation[NOOFTARGMARK][2];
+extern const float dm[NOOFOBS-1];
+extern const uint8_t hmm_labels[NOOFSTATES][NOOFOBS];
+
+#endif
+
