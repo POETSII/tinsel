37c01f962e1db9a7523f61c6b9f02b776b78b2b6
Matthew Naylor <mn416@cam.ac.uk>
2020-09-23 16:46:04 +0100

    Optional SRAMs: qsys components

--- de5/setup-qdr.sh
+++ de5/setup-qdr.sh
@@ -9,10 +9,12 @@ rm -f S5_DDR3_QSYS.qsys
 if [ "$EnableQDRIISRAM" == "False" ]; then
   echo "Disabling QDRII SRAMs"
   ln -s SoC_No_QDR.qsys S5_DDR3_QSYS.qsys
+  ln -s DE5Top_hw_No_QDR.tcl DE5Top_hw.tcl
   echo '' > setup-qdr.tcl
 else
   echo "Enabling QDRII SRAMs"
   ln -s SoC_With_QDR.qsys S5_DDR3_QSYS.qsys
+  ln -s DE5Top_hw_With_QDR.tcl DE5Top_hw.tcl
   echo 'set_global_assignment -name VERILOG_MACRO "ENABLE_QDR_SRAMs=1"' > \
     setup-qdr.tcl
 fi
