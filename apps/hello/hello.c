43d3626febb0f44639c16ebbdabd2b900a79ff9d
Coral Westoby <tgmp2@cam.ac.uk>
2022-01-06 13:35:20 +0000

    without data section a app runs.

--- apps/hello/hello.c
+++ apps/hello/hello.c
@@ -1,9 +1,20 @@
 // SPDX-License-Identifier: BSD-2-Clause
 #include <tinsel.h>
 
+INLINE void sendb(char c) {
+  while (tinselUartTryPut(c) == 0);
+}
+
+
 int main()
 {
-  unsigned int me = tinselId();
-  printf("Hello from thread 0x%x\n", me);
+  sendb('h');
+  sendb('e');
+  sendb('l');
+  sendb('l');
+  sendb('o');
+  sendb('\n');
+  // unsigned int me = tinselId();
+  // printf("Hello from thread 0x%x\n", me);
   return 0;
 }
