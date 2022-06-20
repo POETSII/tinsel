379f654cbd0683426fb513e600c357a2ccc6cded
Coral Westoby <cmw97@cl.cam.ac.uk>
2022-06-20 10:43:13 +0100

    Timing clean; south port still fails.

--- de10-pro/DE10_Pro.v
+++ de10-pro/DE10_Pro.v
@@ -386,16 +386,22 @@ wire [11:0] ddr4_status;
         // .nic_d_s10_100gmac_a_serial_lanes_tx_serial                    (QSFP28D_TX_p),                    //  output,   width = 4,      nic_a_s10_100gmac_a_serial_lanes.tx_serial
         // .nic_d_s10_100gmac_a_serial_lanes_rx_serial                    (QSFP28D_RX_p),                    //   input,   width = 4,                                      .rx_serial
 
-        .alt_ehipc2_0_i_clk_ref_clk                                    (QSFP28A_REFCLK_p),                                    //   input,   width = 1,                alt_ehipc2_0_i_clk_ref.clk
-        .alt_ehipc2_0_o_tx_serial_o_tx_serial                          (QSFP28A_TX_p),                          //  output,   width = 4,              alt_ehipc2_0_o_tx_serial.o_tx_serial
-        .alt_ehipc2_0_i_rx_serial_i_rx_serial                          (QSFP28A_RX_p),                          //   input,   width = 4,              alt_ehipc2_0_i_rx_serial.i_rx_serial
+        // .alt_ehipc2_0_i_clk_ref_clk                                    (QSFP28A_REFCLK_p),                                    //   input,   width = 1,                alt_ehipc2_0_i_clk_ref.clk
+        // .alt_ehipc2_0_o_tx_serial_o_tx_serial                          (QSFP28A_TX_p),                          //  output,   width = 4,              alt_ehipc2_0_o_tx_serial.o_tx_serial
+        // .alt_ehipc2_0_i_rx_serial_i_rx_serial                          (QSFP28A_RX_p),                          //   input,   width = 4,              alt_ehipc2_0_i_rx_serial.i_rx_serial
+
+        // .alt_ehipc2_1_i_clk_ref_clk                                    (QSFP28B_REFCLK_p),                                    //   input,   width = 1,                alt_ehipc2_0_i_clk_ref.clk
+        // .alt_ehipc2_1_o_tx_serial_o_tx_serial                          (QSFP28B_TX_p),                          //  output,   width = 4,              alt_ehipc2_0_o_tx_serial.o_tx_serial
+        // .alt_ehipc2_1_i_rx_serial_i_rx_serial                          (QSFP28B_RX_p),                          //   input,   width = 4,              alt_ehipc2_0_i_rx_serial.i_rx_serial
         .atx_pll_dualclk_0_clock_sink_clk                              (QSFP28A_REFCLK_p),                              //   input,   width = 1,          atx_pll_dualclk_0_clock_sink.clk
-
-        .alt_ehipc2_1_i_clk_ref_clk                                    (QSFP28B_REFCLK_p),                                    //   input,   width = 1,                alt_ehipc2_0_i_clk_ref.clk
-        .alt_ehipc2_1_o_tx_serial_o_tx_serial                          (QSFP28B_TX_p),                          //  output,   width = 4,              alt_ehipc2_0_o_tx_serial.o_tx_serial
-        .alt_ehipc2_1_i_rx_serial_i_rx_serial                          (QSFP28B_RX_p),                          //   input,   width = 4,              alt_ehipc2_0_i_rx_serial.i_rx_serial
+        .alt_e100s10_north_clk_ref_clk                                 (QSFP28A_REFCLK_p),                                 //   input,   width = 1,             alt_e100s10_north_clk_ref.clk
+        .alt_e100s10_north_serial_lanes_tx_serial                      (QSFP28A_TX_p),                      //  output,   width = 4,        alt_e100s10_south_serial_lanes.tx_serial
+        .alt_e100s10_north_serial_lanes_rx_serial                      (QSFP28A_RX_p),
+                             //   input,   width = 4,                                      .rx_serial
         .atx_pll_dualclk_1_clock_sink_clk                              (QSFP28B_REFCLK_p),                              //   input,   width = 1,          atx_pll_dualclk_0_clock_sink.clk
-
+        .alt_e100s10_south_serial_lanes_tx_serial                      (QSFP28B_TX_p),                      //  output,   width = 4,        alt_e100s10_south_serial_lanes.tx_serial
+        .alt_e100s10_south_serial_lanes_rx_serial                      (QSFP28B_RX_p),                      //   input,   width = 4,                                      .rx_serial
+        .alt_e100s10_south_clk_ref_clk                                 (QSFP28B_REFCLK_p),                                  //   input,   width = 1,             alt_e100s10_south_clk_ref.clk
 
 
         .tinsel_0_temperature_val(178),
