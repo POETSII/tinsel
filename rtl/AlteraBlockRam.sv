08fe2166f7ae863722b0060a0b75a54f2dbfc3c5
Coral Westoby <cmw97@cl.cam.ac.uk>
2022-08-03 17:08:11 +0100

    revert switch to alttera_syncram.

--- rtl/AlteraBlockRam.sv
+++ rtl/AlteraBlockRam.sv
@@ -40,7 +40,7 @@ module AlteraBlockRam (
   parameter RD_DURING_WR = "OLD_DATA";     // Or: "DONT_CARE"
   parameter DO_REG       = "UNREGISTERED"; // Or: "CLOCK0"
   parameter INIT_FILE    = "UNUSED";
-  parameter DEV_FAMILY   = "Stratix 10";
+  parameter DEV_FAMILY   = "Stratix V";
   parameter STYLE        = "AUTO";
 
   input  CLK;
@@ -61,59 +61,52 @@ module AlteraBlockRam (
 // synopsys translate_on
 `endif
 
-  altera_syncram  altera_syncram_component (
-               .address_a (ADDR_A),
-               .clock0 (CLK),
-               .data_a (DI_A),
-               .wren_a (EN_A & WE_A),
-               .q_a (DO_A),
-               .aclr0 (1'b0),
-               .aclr1 (1'b0),
-               .address2_a (1'b1),
-               .addressstall_a (1'b0),
-               .byteena_a (1'b1),
-               .clocken0 (1'b1),
-               .eccencbypass (1'b0),
-               .eccencparity (8'b0),
-               .eccstatus (),
-               .rden_a (1'b1),
-               .sclr (1'b0));
-   defparam
-       altera_syncram_component.operation_mode  = "SINGLE_PORT",
-
-       // port A
-       altera_syncram_component.width_a  = DATA_WIDTH,
-       altera_syncram_component.widthad_a  = ADDR_WIDTH,
-       altera_syncram_component.widthad2_a  = ADDR_WIDTH,
-       altera_syncram_component.numwords_a  = NUM_ELEMS,
-       altera_syncram_component.outdata_reg_a  = DO_REG, // UNREGISTERED : CLOCK0
-       altera_syncram_component.outdata_aclr_a  = "NONE",
-       altera_syncram_component.outdata_sclr_a  = "NONE",
-       altera_syncram_component.address_aclr_a  = "NONE",
-       altera_syncram_component.width_byteena_a  = 1,
-
-       altera_syncram_component.intended_device_family  = "Stratix 10",
-       altera_syncram_component.ram_block_type  = STYLE,
-       altera_syncram_component.byte_size  = "8",
-
-       altera_syncram_component.init_file = INIT_FILE,
-       altera_syncram_component.init_file_layout = "PORT_A",
-
-       altera_syncram_component.clock_enable_input_a  = "BYPASS",
-       altera_syncram_component.clock_enable_output_a  = "BYPASS",
-       altera_syncram_component.clock_enable_core_a  = "BYPASS",
-
-       altera_syncram_component.read_during_write_mode_port_a  = RD_DURING_WR,
-
-       altera_syncram_component.enable_ecc = "FALSE",
-       altera_syncram_component.ecc_pipeline_stage_enabled = "FALSE",
-       altera_syncram_component.enable_ecc_encoder_bypass = "FALSE",
-
-       altera_syncram_component.enable_coherent_read = "FALSE",
-       altera_syncram_component.enable_force_to_zero  = "FALSE",
-
-       altera_syncram_component.width_eccencparity = "8",
-       altera_syncram_component.optimization_option = "AUTO";
+  altsyncram altsyncram_component (
+        .address_a (WR_ADDR),
+        .byteena_a (BE_WIDTH == 1 ? 1'b1 : BE),
+        .clock0 (CLK),
+        .data_a (DI),
+        .wren_a (WE),
+        .address_b (RD_ADDR),
+        .q_b (DO),
+        .aclr0 (1'b0),
+        .aclr1 (1'b0),
+        .addressstall_a (1'b0),
+        .addressstall_b (1'b0),
+        .byteena_b (-1),
+        .clock1 (1'b1),
+        .clocken0 (1'b1),
+        .clocken1 (1'b1),
+        .clocken2 (1'b1),
+        .clocken3 (1'b1),
+        .data_b (-1),
+        .eccstatus (),
+        .q_a (),
+        .rden_a (1'b1),
+        .rden_b (1'b1),
+        .wren_b (1'b0));
+  defparam
+    altsyncram_component.address_aclr_b = "NONE",
+    altsyncram_component.address_reg_b = "CLOCK0",
+    altsyncram_component.clock_enable_input_a = "BYPASS",
+    altsyncram_component.clock_enable_input_b = "BYPASS",
+    altsyncram_component.clock_enable_output_b = "BYPASS",
+    altsyncram_component.init_file = INIT_FILE,
+    altsyncram_component.intended_device_family = DEV_FAMILY,
+    altsyncram_component.lpm_type = "altsyncram",
+    altsyncram_component.numwords_a = NUM_ELEMS,
+    altsyncram_component.numwords_b = NUM_ELEMS,
+    altsyncram_component.operation_mode = "DUAL_PORT",
+    altsyncram_component.outdata_aclr_b = "NONE",
+    altsyncram_component.outdata_reg_b = DO_REG,
+    altsyncram_component.power_up_uninitialized = "FALSE",
+    altsyncram_component.read_during_write_mode_mixed_ports = RD_DURING_WR,
+    altsyncram_component.widthad_a = ADDR_WIDTH,
+    altsyncram_component.widthad_b = ADDR_WIDTH,
+    altsyncram_component.width_a = DATA_WIDTH,
+    altsyncram_component.width_b = DATA_WIDTH,
+    altsyncram_component.width_byteena_a = BE_WIDTH,
+    altsyncram_component.ram_block_type = STYLE;
 
 endmodule
 
@@ -146,7 +139,7 @@ module AlteraBlockRamTrueMixed (
   parameter RD_DURING_WR = "OLD_DATA";     // Or: "DONT_CARE"
   parameter DO_REG_A     = "UNREGISTERED"; // Or: "CLOCK0"
   parameter DO_REG_B     = "UNREGISTERED"; // Or: "CLOCK0"
-  parameter DEV_FAMILY   = "Stratix 10";
+  parameter DEV_FAMILY   = "Stratix V";
   parameter INIT_FILE    = "UNUSED";
   parameter STYLE        = "AUTO";
 
@@ -171,89 +164,60 @@ module AlteraBlockRamTrueMixed (
 // synopsys translate_on
 `endif
 
-  altera_syncram  altera_syncram_component (
-               .address_a (ADDR_A),
-               .address_b (ADDR_B),
-               .clock0 (CLK),
-               .data_a (DI_A),
-               .data_b (DI_B),
-               .wren_a (EN_A & WE_A),
-               .wren_b (EN_B & WE_B),
-               .q_a (DO_A),
-               .q_b (DO_B),
-               .aclr0 (1'b0),
-               .aclr1 (1'b0),
-               .address2_a (1'b1),
-               .address2_b (1'b1),
-               .addressstall_a (1'b0),
-               .addressstall_b (1'b0),
-               // .byteena_a ($unsigned(~0)),
-               // .byteena_b ($unsigned(~0)),
-               // .clock1 (1'b0),
-               .clocken0 (1'b1),
-               // .clocken1 (1'b0),
-               .eccencbypass (1'b0),
-               .eccencparity (8'b0),
-               .eccstatus (),
-               .rden_a (1'b1),
-               .rden_b (1'b1),
-               .sclr (1'b0));
-   defparam
-       altera_syncram_component.operation_mode  = "BIDIR_DUAL_PORT",
-
-       // port A
-       altera_syncram_component.width_a  = DATA_WIDTH_A,
-       altera_syncram_component.widthad_a  = ADDR_WIDTH_A,
-       altera_syncram_component.widthad2_a  = ADDR_WIDTH_A,
-       altera_syncram_component.numwords_a  = NUM_ELEMS_A,
-       altera_syncram_component.outdata_reg_a  = DO_REG_A, // UNREGISTERED : CLOCK0
-       altera_syncram_component.outdata_aclr_a  = "NONE",
-       altera_syncram_component.outdata_sclr_a  = "NONE",
-       altera_syncram_component.address_aclr_a  = "NONE",
-       // altera_syncram_component.width_byteena_a  = DATA_WIDTH_A/8,
-
-       altera_syncram_component.width_b  = DATA_WIDTH_B,
-       altera_syncram_component.widthad_b  = ADDR_WIDTH_B,
-       altera_syncram_component.widthad2_b  = ADDR_WIDTH_B,
-       altera_syncram_component.numwords_b  = NUM_ELEMS_B,
-       altera_syncram_component.outdata_reg_b  = DO_REG_B,
-       altera_syncram_component.indata_reg_b  = "CLOCK0",
-       altera_syncram_component.address_reg_b  = "CLOCK0",
-       altera_syncram_component.byteena_reg_b  = "CLOCK0",
-       altera_syncram_component.outdata_aclr_b  = "NONE",
-       altera_syncram_component.outdata_sclr_b  = "NONE",
-       altera_syncram_component.address_aclr_b  = "NONE",
-       // altera_syncram_component.width_byteena_b  = DATA_WIDTH_B/8,
-
-       altera_syncram_component.intended_device_family  = "Stratix 10",
-       altera_syncram_component.ram_block_type  = STYLE,
-       altera_syncram_component.byte_size  = "8",
-
-       altera_syncram_component.read_during_write_mode_mixed_ports  = "DONT_CARE", // only supported value RD_DURING_WR,
-
-       altera_syncram_component.init_file = INIT_FILE,
-       altera_syncram_component.init_file_layout = "PORT_A",
-
-       altera_syncram_component.clock_enable_input_a  = "BYPASS",
-       altera_syncram_component.clock_enable_output_a  = "BYPASS",
-       altera_syncram_component.clock_enable_core_a  = "BYPASS",
-
-       altera_syncram_component.clock_enable_input_b  = "BYPASS",
-       altera_syncram_component.clock_enable_output_b  = "BYPASS",
-       altera_syncram_component.clock_enable_core_b  = "BYPASS",
-
-       altera_syncram_component.read_during_write_mode_port_a  = RD_DURING_WR,
-       altera_syncram_component.read_during_write_mode_port_b  = RD_DURING_WR,
-
-       altera_syncram_component.enable_ecc = "FALSE",
-       altera_syncram_component.ecc_pipeline_stage_enabled = "FALSE",
-       altera_syncram_component.enable_ecc_encoder_bypass = "FALSE",
-
-       altera_syncram_component.enable_coherent_read = "FALSE",
-       altera_syncram_component.enable_force_to_zero  = "FALSE",
-
-       altera_syncram_component.width_eccencparity = "8",
-       altera_syncram_component.optimization_option = "AUTO";
+  altsyncram altsyncram_component (
+        .address_a (ADDR_A),
+        .address_b (ADDR_B),
+        .byteena_b (1'b1),
+        .clock0 (CLK),
+        .data_a (DI_A),
+        .data_b (DI_B),
+        .wren_a (EN_A & WE_A),
+        .wren_b (EN_B & WE_B),
+        .q_a (DO_A),
+        .q_b (DO_B),
+        .aclr0 (1'b0),
+        .aclr1 (1'b0),
+        .addressstall_a (1'b0),
+        .addressstall_b (1'b0),
+        .byteena_a (1'b1),
+        .clock1 (1'b1),
+        .clocken0 (1'b1),
+        .clocken1 (1'b1),
+        .clocken2 (1'b1),
+        .clocken3 (1'b1),
+        .eccstatus (),
+        .rden_a (1'b1),
+        .rden_b (1'b1));
+  defparam
+    altsyncram_component.address_reg_b = "CLOCK0",
+    altsyncram_component.clock_enable_input_a = "BYPASS",
+    altsyncram_component.clock_enable_input_b = "BYPASS",
+    altsyncram_component.clock_enable_output_a = "BYPASS",
+    altsyncram_component.clock_enable_output_b = "BYPASS",
+    altsyncram_component.init_file = INIT_FILE,
+    altsyncram_component.init_file_layout = "PORT_A",
+    altsyncram_component.indata_reg_b = "CLOCK0",
+    altsyncram_component.intended_device_family = DEV_FAMILY,
+    altsyncram_component.lpm_type = "altsyncram",
+    altsyncram_component.numwords_a = NUM_ELEMS_A,
+    altsyncram_component.numwords_b = NUM_ELEMS_B,
+    altsyncram_component.operation_mode = "BIDIR_DUAL_PORT",
+    altsyncram_component.outdata_aclr_a = "NONE",
+    altsyncram_component.outdata_aclr_b = "NONE",
+    altsyncram_component.outdata_reg_a = DO_REG_A,
+    altsyncram_component.outdata_reg_b = DO_REG_B,
+    altsyncram_component.power_up_uninitialized = "FALSE",
+    altsyncram_component.read_during_write_mode_mixed_ports = RD_DURING_WR,
+    altsyncram_component.read_during_write_mode_port_a = "NEW_DATA_NO_NBE_READ",
+    altsyncram_component.read_during_write_mode_port_b = "NEW_DATA_NO_NBE_READ",
+    altsyncram_component.widthad_a = ADDR_WIDTH_A,
+    altsyncram_component.widthad_b = ADDR_WIDTH_B,
+    altsyncram_component.width_a = DATA_WIDTH_A,
+    altsyncram_component.width_b = DATA_WIDTH_B,
+    altsyncram_component.width_byteena_a = 1,
+    altsyncram_component.width_byteena_b = 1,
+    altsyncram_component.wrcontrol_wraddress_reg_b = "CLOCK0",
+    altsyncram_component.ram_block_type = STYLE;
 
 endmodule
 
@@ -367,7 +331,7 @@ module AlteraBlockRamTrueMixedBE (
     altsyncram_component.widthad_b = ADDR_WIDTH_B,
     altsyncram_component.width_a = DATA_WIDTH_A,
     altsyncram_component.width_b = DATA_WIDTH_B,
-    altsyncram_component.width_byteena_a = BE_WIDTH,
+    altsyncram_component.width_byteena_a = 1,
     altsyncram_component.width_byteena_b = BE_WIDTH,
     altsyncram_component.wrcontrol_wraddress_reg_b = "CLOCK0",
     altsyncram_component.ram_block_type = STYLE;
